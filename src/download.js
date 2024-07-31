d4bb39feaa565a984800b8d2da6025fa2483c03f
Ayden Smith <s_ayden@ymail.com>
2024-07-31 12:32:00 +1000

    add onWholeMsg function

--- src/download.js
+++ src/download.js
@@ -5,21 +5,42 @@ const Buffer = require("buffer").Buffer;
 const tracker = require("./tracker");
 
 module.exports = (torrent) => {
-    // Get the list of peers from the tracker
-    tracker.getPeers(torrent, (peers) => {
-        // For each peer, initiate the download
-        peers.forEach(download);
-    });
+  // Get the list of peers from the tracker
+  tracker.getPeers(torrent, (peers) => {
+    // For each peer, initiate the download
+    peers.forEach(download);
+  });
 };
 
 function download(peer) {
-    const socket = net.Socket();
-    socket.on("error", console.log);
-    socket.connect(peer.port, peer.ip, () => {
-        // Once connected to the peer, write data to the socket
-        // socket.write(...)
-    });
-    socket.on("data", (data) => {
-        // Handle the response data received from the peer
+  const socket = net.Socket();
+  socket.on("error", console.log);
+  socket.connect(peer.port, peer.ip, () => {
+    // Once connected to the peer, write data to the socket
+    // socket.write(...)
+  });
+  onWholeMsg(socket, (data) => {
+    // handle response here
+  });
+}
+
+// This function handles receiving data from the socket and processing it as complete messages
+function onWholeMsg(socket, callback) {
+    let savedBuf = Buffer.alloc(0);
+    let handshake = true;
+
+    socket.on("data", (recvBuf) => {
+        // Calculate the length of a whole message
+        const msgLen = () =>
+            handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+        savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+        // Process complete messages in the buffer
+        while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+            // Pass the complete message to the callback function
+            callback(savedBuf.slice(0, msgLen()));
+            savedBuf = savedBuf.slice(msgLen());
+            handshake = false;
+        }
     });
 }
